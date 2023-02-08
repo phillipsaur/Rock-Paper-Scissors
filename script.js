f2a764c4b18c4700bbbde84ce9d0ec0af756e524
phillipsaur <phillipdthai@gmail.com>
2023-02-07 22:42:36 -0800

    Added pseudocode and function framework

---
+++ script.js
@@ -0,0 +1,17 @@
+
+//Write a function called getComputerChoice that will randomly return either 'Rock', 'Paper', or 'Scissors'
+function getComputerChoice(computerSelection) {
+    
+}
+
+
+//Write a funcation to get the player case-insensitive input
+function getPlayerChoice() {
+
+}
+
+
+//Write a function that plays one round of RPS then determines the winnner and returns the winner.
+function whoWon(computerSelection, playerSelection) {
+
+}
