908ec08400dafe03ff2d361f07b1de8b91493dd7
phillipsaur <phillipdthai@gmail.com>
2023-02-12 01:08:03 -0800

    Refactored code, but still not working.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const playerSelection = "rOck";
-const computerSelection = "getComputerChoice()";
+let playerSelection = "getPlayerSelection()";
+let computerSelection = "getComputerChoice()";
 const RPS = ["Rock","Paper","Scissors"];
 
 
@@ -9,51 +9,26 @@ function getComputerChoice() {
     return RPS[randomNumber];
 }
 
+//Write a function to prompt the user to make a selection for Rock, Paper, Scissors.
+function getPlayerSelection() {
+    return playerSelection = prompt("Rock, Paper or Scissors?");
+}
+
 
 //Write a function that plays one round of RPS then determines the winnner and returns the winner.
 function whoWon(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     
-    switch(playerSelection) {
-        case "rock":
-            switch(computerSelection){
-                case "Rock":
-                    console.log("Tie!");
-                    break;
-                case "Paper":
-                    console.log("Computer Wins!");
-                    break;
-                case "Scissors":
-                    console.log("Player Wins!");
-                    break;
-            }
-            break;
-        case "paper":
-            switch(computerSelection){
-                case "Rock":
-                    console.log("Player Wins!");
-                    break;
-                case "Paper":
-                    console.log("Tie!");
-                    break;
-                case "Scissors":
-                    console.log("Computer Wins!");
-                    break;
-            }
-            break;
-        case "scissors":
-            switch(computerSelection){
-                case "Rock":
-                    console.log("Computer Wins!");
-                    break;
-                case "Paper":
-                    console.log("Player Wins!");
-                    break;
-                case "Scissors":
-                    console.log("Tie!");
-                    break;
-            }
-            break;
+    if (playerSelection === computerSelection) {
+        return "Tie!";
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "Player Wins! Rock beats Scissors";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "Player Wins! Paper beats Rock";
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "Player Wins! Scissors beats Paper";
+    } else {
+        return "Computer Wins!"
     }
 }
 
